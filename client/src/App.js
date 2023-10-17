aeb4a072b875dac8c5aea747c6ce2b40f84cbbf3
Zo3rb <ali.f90@outlook.com>
2023-10-17 17:21:36 +0300

    initializied the API and tested in client.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,18 @@
 import { Box, Text } from "@chakra-ui/react";
+import axios from "axios";
+import api from "./api";
+import { useEffect } from "react";
 
 function App() {
+  async function fetchData() {
+    const { data } = await api.get("/health-check");
+    console.log(data);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <Box p={4} bg="teal.500">
       <Text fontSize="xl" color="white">
